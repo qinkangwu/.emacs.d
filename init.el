012148a83710a3e4d21542ce59e0d2e51432c8aa
admin <qinkangwu@xs1h.com>
2017-12-06 17:30:52 +0800

    asd

--- init.el
+++ init.el
@@ -1,3 +1,53 @@
+;; 添加melpa作为插件源
+(when (>= emacs-major-version 24)
+     (require 'package)
+     (package-initialize)
+     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+		      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+
+;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+
+ ;; cl - Common Lisp Extension
+ (require 'cl)
+
+ ;; Add Packages
+ (defvar my/packages '(
+		;; --- Auto-completion ---
+		company
+		;; --- Better Editor ---
+		hungry-delete
+		swiper
+		counsel
+		smartparens
+		;; --- Major Mode ---
+		js2-mode
+		;; --- Minor Mode ---
+		nodejs-repl
+		exec-path-from-shell
+		;; --- Themes ---
+		monokai-theme
+		;; solarized-theme
+		) "Default packages")
+
+ (setq package-selected-packages my/packages)
+
+ (defun my/packages-installed-p ()
+     (loop for pkg in my/packages
+	   when (not (package-installed-p pkg)) do (return nil)
+	   finally (return t)))
+
+ (unless (my/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg my/packages)
+       (when (not (package-installed-p pkg))
+	 (package-install pkg))))
+
+ ;; Find Executable Path on OS X
+ (when (memq window-system '(mac ns))
+   (exec-path-from-shell-initialize))
+
+
 ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
 (tool-bar-mode -1)
 
@@ -28,7 +78,57 @@
 ;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
 (global-set-key (kbd "<f2>") 'open-init-file)
 
-; 开启全局 Company 补全
+;; 开启全局 Company 补全
 (global-company-mode 1)
 
-(setq backup-directory-alist (quote (("." . "~/.backups"))))
+;; 关闭默认备份
+(setq make-backup-files nil)
+
+;;显示最近打开的文件
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 10)
+
+;;使emcas支持选中替换的模式
+(delete-selection-mode 1)
+
+;;使emacs默认全屏
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;;高亮当前行
+(global-hl-line-mode 1)
+
+;;自动括号匹配
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;;安装主题并使用主题
+(add-to-list 'my/packages 'creamsody-theme)
+(load-theme 'creamsody 1)
+
+;;饥饿删除(删除多余空格字符)
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (js3-mode swiper-helm company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js3-mode))
+       auto-mode-alist))
+
+;; 解决mac找不到nodejs可执行文件的问题
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
