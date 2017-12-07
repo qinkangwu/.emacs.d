3cec7c1cd321c016e6336b28e3a059ac4cf580a2
admin <qinkangwu@xs1h.com>
2017-12-07 14:47:52 +0800

    asd

--- init.el
+++ init.el
@@ -48,66 +48,6 @@
    (exec-path-from-shell-initialize))
 
 
-;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
-(tool-bar-mode -1)
-
-;; 关闭文件滑动控件
-(scroll-bar-mode -1)
-
-;; 显示行号
-(global-linum-mode 1)
-
-;; 更改光标的样式（不能生效，解决方案见第二集）
-(setq cursor-type 'bar)
-
-;; 关闭启动帮助画面
-(setq inhibit-splash-screen 1)
-
-;; 关闭缩进 (第二天中被去除)
-;; (electric-indent-mode -1)
-
-;; 更改显示字体大小 16pt
-;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
-(set-face-attribute 'default nil :height 160)
-
-;; 快速打开配置文件
-(defun open-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-
-;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
-(global-set-key (kbd "<f2>") 'open-init-file)
-
-;; 开启全局 Company 补全
-(global-company-mode 1)
-
-;; 关闭默认备份
-(setq make-backup-files nil)
-
-;;显示最近打开的文件
-(require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-item 10)
-
-;;使emcas支持选中替换的模式
-(delete-selection-mode 1)
-
-;;使emacs默认全屏
-(setq initial-frame-alist (quote ((fullscreen . maximized))))
-
-;;高亮当前行
-(global-hl-line-mode 1)
-
-;;自动括号匹配
-(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
-
-;;安装主题并使用主题
-(add-to-list 'my/packages 'creamsody-theme)
-(load-theme 'creamsody 1)
-
-;;饥饿删除(删除多余空格字符)
-(require 'hungry-delete)
-(global-hungry-delete-mode)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -132,3 +72,45 @@
 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
+
+
+
+
+
+
+
+
+
+;;分包
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lib/")
+(require 'init-ui)
+
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+;; 开启全局 Company 补全
+(global-company-mode 1)
+
+;; 关闭默认备份
+(setq make-backup-files nil)
+
+;;显示最近打开的文件
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 10)
+
+;;使emcas支持选中替换的模式
+(delete-selection-mode 1)
+;;自动括号匹配
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+;;饥饿删除(删除多余空格字符)
+(require 'hungry-delete)
+(global-hungry-delete-mode)
---
+++ lib/init-ui.el
@@ -0,0 +1,33 @@
+;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
+(tool-bar-mode -1)
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;; 显示行号
+(global-linum-mode 1)
+
+;; 更改光标的样式（不能生效，解决方案见第二集）
+(setq cursor-type 'bar)
+
+;; 关闭启动帮助画面
+(setq inhibit-splash-screen 1)
+
+;; 关闭缩进 (第二天中被去除)
+;; (electric-indent-mode -1)
+
+;; 更改显示字体大小 16pt
+;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+(set-face-attribute 'default nil :height 160)
+
+;;使emacs默认全屏
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;;高亮当前行
+(global-hl-line-mode 1)
+
+;;安装主题并使用主题
+(add-to-list 'my/packages 'creamsody-theme)
+(load-theme 'creamsody 1)
+
+(provide 'init-ui)
