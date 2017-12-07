2d990af9e915bb0185ec4a34defb95d5745cd9d2
admin <qinkangwu@xs1h.com>
2017-12-07 17:37:32 +0800

    asd

--- lib/init-better-default.el
+++ lib/init-better-default.el
@@ -4,6 +4,16 @@
 ;; 关闭默认备份
 (setq make-backup-files nil)
 
+;;自动加载外部修改过的文件
+(global-auto-revert-mode 1)
+
+;;关闭自动保存
+(setq auto-save-default nil)
+
+;;将光标自动定位到新窗口
+(require 'popwin)
+(popwin-mode 1)
+
 ;;使emcas支持选中替换的模式
 (delete-selection-mode 1)
 ;;自动括号匹配
@@ -12,4 +22,6 @@
 (require 'hungry-delete)
 (global-hungry-delete-mode)
 
+;;用yn代替yes no
+(fset 'yes-or-no-p 'y-or-n-p)
 (provide 'init-better-default)
